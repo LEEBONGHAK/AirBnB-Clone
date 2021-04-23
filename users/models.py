a0ac85c9ccd1e2760b9051633c1190fe7a849118
octo <dlqhdgkr456@gmail.com>
2021-04-23 15:53:34 +0900

    3.0 Replacing Default User

--- users/models.py
+++ users/models.py
@@ -1,3 +1,6 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+# Create your models here.(데이터가 보여지는 모습)
+class User(AbstractUser):
+    pass
