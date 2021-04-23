c430007e8d1f9d33689c013c81c1632fed074b4b
octo <dlqhdgkr456@gmail.com>
2021-04-23 20:09:01 +0900

    3.4 Falling in Love with Admin Panel

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-@admin.register(models.User)
+# admin 패널에서 User을 보고 싶어 User을 컨트롤한 클래스가 아래가 될 거야라는 의미
+@admin.register(models.User)  # decorator - class바로 위에 있어야 인식 됨(줄이 띄어져 있어도 안됨)
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+
+    """ Custom User Admin """
+
+    list_display = ("username", "email", "gender", "langauge", "currency", "superhost")
+    list_filter = ("superhost", "langauge", "currency")  # 하나만 있을 경우 ',' 붙여줘야함
--- users/models.py
+++ users/models.py
@@ -5,7 +5,7 @@ from django.db import models
 # Model에 뭘 쓰든 장고가 알아서 form을 만들어 데이터베이스에 migration과 함꼐 이 form에 필요한 정보를 요청할 것임
 class User(AbstractUser):
 
-    """ Custom User Modeel """
+    """ Custom User Model """
 
     GENDER_MALE = "male"
     GENDER_FEMALE = "female"
