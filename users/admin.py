c04ac3b71db4115f0b199c5c5bb7c24df0c3a8bf
octo <dlqhdgkr456@gmail.com>
2021-04-23 20:22:08 +0900

    3.5 UserAdmin + CustomAdmin

--- users/admin.py
+++ users/admin.py
@@ -1,12 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 # Register your models here.
 # admin 패널에서 User을 보고 싶어 User을 컨트롤한 클래스가 아래가 될 거야라는 의미
 @admin.register(models.User)  # decorator - class바로 위에 있어야 인식 됨(줄이 띄어져 있어도 안됨)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
 
     """ Custom User Admin """
 
-    list_display = ("username", "email", "gender", "langauge", "currency", "superhost")
-    list_filter = ("superhost", "langauge", "currency")  # 하나만 있을 경우 ',' 붙여줘야함
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profile",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "langauge",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
