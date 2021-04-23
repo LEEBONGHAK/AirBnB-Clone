e3a5964897a24a9e805befcba323de0d476f800c
octo <dlqhdgkr456@gmail.com>
2021-04-23 16:12:01 +0900

    3.1 Introduction to the User Model

--- users/models.py
+++ users/models.py
@@ -2,5 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.(데이터가 보여지는 모습)
+# Model에 뭘 쓰든 장고가 알아서 form을 만들어 데이터베이스에 migration과 함꼐 이 form에 필요한 정보를 요청할 것임
 class User(AbstractUser):
-    pass
+    bio = models.TextField(default="")
