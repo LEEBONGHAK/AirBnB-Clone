55fee8390ed0583acbaf60cdf9cdb575cf29d513
octo <dlqhdgkr456@gmail.com>
2021-04-25 23:16:18 +0900

    4.5 Meta Class and Photos Model

--- rooms/models.py
+++ rooms/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django_countries.fields import CountryField
-from pkg_resources import parse_version
 from core import models as core_models
-from users import models as user_models
 
 
 class AbstractItem(core_models.TimeStampdModel):
@@ -22,28 +20,46 @@ class RoomType(AbstractItem):
 
     """ RoomType Object Definition """
 
-    pass
+    class Meta:
+        verbose_name = "Room Types"
+        ordering = ["created"]
 
 
 class Amenity(AbstractItem):
 
     """ Amenity Object Definition """
 
-    pass
+    # Meta Class : 모델 내의 모든 class들 안에 있는 class / 많은 것들을 설정할 수 있다.
+    class Meta:
+        verbose_name_plural = "Amenities"
 
 
 class Facility(AbstractItem):
 
     """ Facility Model Definition """
 
-    pass
+    class Meta:
+        verbose_name_plural = "Facilities"
 
 
 class HouseRule(AbstractItem):
 
     """ HouseRule Model Definition """
 
-    pass
+    class Meta:
+        verbose_name = "House Rules"
+
+
+class Photo(core_models.TimeStampdModel):
+
+    """ Photo Model Definition """
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
 
 
 # Create your models here.
@@ -70,12 +86,12 @@ class Room(core_models.TimeStampdModel):
     # 일대다 관계를 만들어 냄
     # on_delete : 만약 User가 삭제(on_delete)되었다면 장고에게 room으로 무엇을 할건지 행동 지정 / 오직 Foreign key에만 해당
     # CASCADE : 폭포수 효과 / 제일 위에서 어떤 일이 발생하고 그것이 아래에 모든 영향을 주는 것
-    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    host = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room_type = models.ForeignKey("RoomType", on_delete=models.SET_NULL, null=True)
     # 다대다 관계를 가지고 있음
-    amenities = models.ManyToManyField(Amenity)
-    facilities = models.ManyToManyField(Facility)
-    house_rules = models.ManyToManyField(HouseRule)
+    amenities = models.ManyToManyField("Amenity", blank=True)
+    facilities = models.ManyToManyField("Facility", blank=True)
+    house_rules = models.ManyToManyField("HouseRule", blank=True)
 
     def __str__(self):
         return self.name
