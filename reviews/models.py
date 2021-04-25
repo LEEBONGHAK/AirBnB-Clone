799e53e7f4265117ff7a4641f6a5aecde2d9e25b
octo <dlqhdgkr456@gmail.com>
2021-04-25 23:33:38 +0900

    5.0 Review Model

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from core import models as core_models
 
 # Create your models here.
+class Review(core_models.TimeStampdModel):
+
+    """ Review Model Definition """
+
+    review = models.TextField()
+    accuracy = models.IntegerField()
+    communication = models.IntegerField()
+    cleanlines = models.IntegerField()
+    location = models.IntegerField()
+    check_in = models.IntegerField()
+    value = models.IntegerField()
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+
+    # 연결된 object에서 value값을 얻을 수 있음
+    def __str__(self):
+        # return sellf.review
+        # return self.room.country
+        # return self.room.name
+        # return self.room.host.username
+        return f"{self.review} - {self.room}"
