0c538abe15116fe2230bc532071037b73525d70b
octo <dlqhdgkr456@gmail.com>
2021-04-28 01:37:02 +0900

    6.0 Room Admin Panel part One

--- rooms/admin.py
+++ rooms/admin.py
@@ -15,7 +15,32 @@ class RoomAdmin(admin.ModelAdmin):
 
     """ Room Admin Definition """
 
-    pass
+    list_display = (
+        "name",
+        "country",
+        "city",
+        "price",
+        "guests",
+        "beds",
+        "bedrooms",
+        "baths",
+        "check_in",
+        "Check_out",
+        "instant_book",
+    )
+
+    list_filter = (
+        "instant_book",
+        "city",
+        "country",
+    )
+
+    # default : icontains (insensitive 포함 => 대소문자 구분 x)
+    # ^ : startswith 등등 많음 => 장고 문서 참조
+    search_fields = (
+        "^city",
+        "host__username",
+    )
 
 
 @admin.register(models.Photo)
