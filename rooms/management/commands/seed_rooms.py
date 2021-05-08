083a548abafaa92af80e08f0073a3320a3e3aae5
octo <dlqhdgkr456@gmail.com>
2021-05-08 15:01:43 +0900

    9.4 seed_rooms part Two

--- rooms/management/commands/seed_rooms.py
+++ rooms/management/commands/seed_rooms.py
@@ -1,6 +1,7 @@
 import random
 from random import choice, seed
 from django.core.management.base import BaseCommand
+from django.contrib.admin.utils import flatten
 from django.db.models.query_utils import select_related_descend
 from django_seed import Seed
 from rooms import models as rooms_models
@@ -41,5 +42,17 @@ class Command(BaseCommand):
                 "baths": lambda x: random.randint(1, 5),
             },
         )
-        seeder.execute()
+        created_rooms = seeder.execute()
+        # 생성된 room 객체의 id 가져오기
+        created_clean = flatten(list(created_rooms.values()))
+        for pk in created_clean:
+            # primary ket로 그 room을 찾기
+            room = rooms_models.Room.objects.get(pk=pk)
+            # 루프를 돌면서 사진을 만들면서 file을 부여
+            for i in range(3, random.randint(10, 17)):
+                rooms_models.Photo.objects.create(
+                    caption=seeder.faker.sentence(),
+                    room=room,
+                    file=f"room_photos/{random.randint(1, 31)}.webp",
+                )
         self.stdout.write(self.style.SUCCESS(f"{number} rooms created!"))
