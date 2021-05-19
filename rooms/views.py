fb3d799e7c4de7dcd28f07b169af1adc7068ffb8
octo <dlqhdgkr456@gmail.com>
2021-05-19 22:34:48 +0900

    10.2 Introduction to Django Templates

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,13 @@
+from datetime import datetime
 from django.shortcuts import render
 
 # Create your views here.
 # url : 요청에 바로 응답하는 방법
 # view: 요청에 답을 하는 방법
 def all_rooms(request):
-    return render(request, "all_rooms")
+    now = datetime.now()
+    hungry = True
+    # context를 이용해 template에 변수를 보낼 수 있다.
+    # logic => {% if %} (파이썬 logic을 사용할 수 있게 만듬)
+    # varibles => {{}}
+    return render(request, "all_rooms.html", context={"now": now, "hungry": hungry})
