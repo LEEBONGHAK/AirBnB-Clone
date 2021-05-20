71fa6e7ede6d1d6d9fe5b467a21cb2c07801f454
octo <dlqhdgkr456@gmail.com>
2021-05-20 23:57:01 +0900

    10.3 Extending Templates part One

--- rooms/views.py
+++ rooms/views.py
@@ -1,13 +1,14 @@
 from datetime import datetime
 from django.shortcuts import render
+from . import models
 
 # Create your views here.
 # url : 요청에 바로 응답하는 방법
 # view: 요청에 답을 하는 방법
 def all_rooms(request):
-    now = datetime.now()
-    hungry = True
+    all_rooms = models.Room.objects.all()
+
     # context를 이용해 template에 변수를 보낼 수 있다.
     # logic => {% if %} (파이썬 logic을 사용할 수 있게 만듬)
     # varibles => {{}}
-    return render(request, "all_rooms.html", context={"now": now, "hungry": hungry})
+    return render(request, "rooms/home.html", context={"rooms": all_rooms})
