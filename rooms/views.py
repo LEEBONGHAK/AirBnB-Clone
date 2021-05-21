ce3b831d522c40b33220b0090a9bbeba55149910
octo <dlqhdgkr456@gmail.com>
2021-05-22 01:10:13 +0900

    11.1 Pagination with Limit and Offset

--- rooms/views.py
+++ rooms/views.py
@@ -6,7 +6,11 @@ from . import models
 # url : 요청에 바로 응답하는 방법
 # view: 요청에 답을 하는 방법
 def all_rooms(request):
-    all_rooms = models.Room.objects.all()
+    page = int(request.GET.get("page", 1))
+    page_size = 10
+    limit = page * page_size
+    offset = limit - page_size
+    all_rooms = models.Room.objects.all()[offset:limit]
 
     # context를 이용해 template에 변수를 보낼 수 있다.
     # logic => {% if %} (파이썬 logic을 사용할 수 있게 만듬)
