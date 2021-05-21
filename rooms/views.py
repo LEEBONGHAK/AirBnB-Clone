f159e6a6ff39ba23264384fc9c73dda2b883acbf
octo <dlqhdgkr456@gmail.com>
2021-05-22 01:27:37 +0900

    11.2 Pages List Navigation

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,4 @@
+from math import ceil
 from datetime import datetime
 from django.shortcuts import render
 from . import models
@@ -6,13 +7,24 @@ from . import models
 # url : 요청에 바로 응답하는 방법
 # view: 요청에 답을 하는 방법
 def all_rooms(request):
-    page = int(request.GET.get("page", 1))
+    page = request.GET.get("page", 1)
+    page = int(page or 1)
     page_size = 10
     limit = page * page_size
     offset = limit - page_size
     all_rooms = models.Room.objects.all()[offset:limit]
+    page_count = ceil(models.Room.objects.count() / page_size)
 
     # context를 이용해 template에 변수를 보낼 수 있다.
     # logic => {% if %} (파이썬 logic을 사용할 수 있게 만듬)
     # varibles => {{}}
-    return render(request, "rooms/home.html", context={"rooms": all_rooms})
+    return render(
+        request,
+        "rooms/home.html",
+        context={
+            "rooms": all_rooms,
+            "page": page,
+            "page_count": page_count,
+            "page_range": range(1, page_count + 1),
+        },
+    )
