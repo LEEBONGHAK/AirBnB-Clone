742b9257b3b007a0232e6572c08cf9da062fd925
octo <dlqhdgkr456@gmail.com>
2021-05-24 22:04:50 +0900

    11.6 Handling Exceptions

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,7 @@
 from math import ceil
 from datetime import datetime
-from django.shortcuts import render
-from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
 from . import models
 
 # Create your views here.
@@ -14,14 +14,19 @@ def all_rooms(request):
     # 데이터 수가 페이지처럼 크지 않을 경우 이러한 것을 orphan이라고 하는데 paginator에 orphan을 정의하여 해결할 수 있음
     paginator = Paginator(room_list, 10, orphans=5)
     # get_page() : 페이지가 음수이거나 초과하면 마지막 또는 처음 페이지를 반환한다. => 핸들링할 요소가 적지만, page의 maximum, minimum을 통제하기 어렵다.
-    # page() : 페이지가 음수이거나 초과하면 에러를 반환한다. => 다양하게 통제할 수 있지만 핸들링을 많이 해주어야함
-    rooms = paginator.page(int(page))
+    # page() : 페이지가 음수이거나 초과하면 에러를 반환한다. => 다양하게 통제할 수 있지만 예외를 통한 핸들링을 많이 해주어야함
+    try:
+        rooms = paginator.page(int(page))
+        return render(
+            request,
+            "rooms/home.html",
+            context={"page": rooms},
+        )
+    except EmptyPage:
+        return redirect("/")
+    except ValueError:
+        return redirect("/")
 
     # context를 이용해 template에 변수를 보낼 수 있다.
     # logic => {% if %} (파이썬 logic을 사용할 수 있게 만듬)
     # varibles => {{}}
-    return render(
-        request,
-        "rooms/home.html",
-        context={"page": rooms},
-    )
