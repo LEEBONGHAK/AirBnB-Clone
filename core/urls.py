7a7804e74b0d4af729dfbe0548af37c1be636d83
octo <dlqhdgkr456@gmail.com>
2021-05-25 14:37:34 +0900

    11.7 Class Based Views

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from rooms import views as room_views
 app_name = "core"
 
 urlpatterns = [
-    path("", room_views.all_rooms, name="home"),
+    # 장고에서 class based view는 view로 변신시켜주는 메소드가 존재
+    path("", room_views.HomeView.as_view(), name="home"),
 ]
