f08bef9c9c65d68ef539bd4a8cf88c15315b6149
octo <dlqhdgkr456@gmail.com>
2021-08-07 17:00:01 +0900

    13.5 Flitering Like a Boss part One

--- rooms/views.py
+++ rooms/views.py
@@ -34,11 +34,11 @@ def search(request):
     city = str.capitalize(city)
     country = request.GET.get("country", "KR")
     room_type = int(request.GET.get("room_type", 0))
-    price = request.GET.get("price", 0)
-    guests = request.GET.get("guests", 0)
-    bedrooms = request.GET.get("bedrooms", 0)
-    beds = request.GET.get("beds", 0)
-    baths = request.GET.get("baths", 0)
+    price = int(request.GET.get("price", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    beds = int(request.GET.get("beds", 0))
+    baths = int(request.GET.get("baths", 0))
     instant = request.GET.get("instant", False)
     super_host = request.GET.get("super_host", False)
     s_amenities = request.GET.getlist("amenities")
@@ -72,8 +72,20 @@ def search(request):
         "facilities": facilities,
     }
 
+    filter_args = {}
+
+    if city != "Anywhere":
+        filter_args["city__startswith"] = city
+
+    filter_args["country"] = country
+
+    if room_type != 0:
+        filter_args["room_type__pk"] = room_type
+
+    rooms = models.Room.objects.filter(**filter_args)
+
     return render(
         request,
         "rooms/search.html",
-        context={**form, **choices},
+        context={**form, **choices, "rooms": rooms},
     )
