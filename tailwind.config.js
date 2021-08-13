cf611996df4600c0eee8299e76e233656141bfb8
octo <dlqhdgkr456@gmail.com>
2021-08-13 20:37:45 +0900

    20.4 Extending Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      // tailwind에 없는 것을 확장해서 사용가능
+      spacing: {
+        // classname: css
+        "25vh": "25vh",
+        "50vh": "50vh",
+        "75vh": "75vh"
+      }
+    },
   },
   variants: {
     extend: {},
