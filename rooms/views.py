fa9200e851259b97bc032a685133a1479b32f99c
octo <dlqhdgkr456@gmail.com>
2021-08-19 12:15:32 +0900

    23.1 Room Photos part Two

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,8 @@
+from django.http import Http404
 from django.views.generic import ListView, DetailView, View, UpdateView
 from django.shortcuts import render
 from django.core.paginator import Paginator
+from users import mixins as user_mixins
 from . import models, forms
 
 # Create your views here.
@@ -109,7 +111,7 @@ class SearchView(View):
         return render(request, "rooms/search.html", {"form": form})
 
 
-class EditRoomView(UpdateView):
+class EditRoomView(user_mixins.LoggedInOnlyView, UpdateView):
 
     model = models.Room
     template_name = "rooms/room_edit.html"
@@ -132,3 +134,27 @@ class EditRoomView(UpdateView):
         "facilities",
         "house_rules",
     )
+
+    def get_object(
+        self, queryset=None
+    ):  # url에서 pk를 받는 UpdateView이 소유권 확인이 힘들하는 점을 보완하기 위해
+        room = super().get_object(queryset=queryset)
+
+        if room.host.pk != self.request.user.pk:
+            raise Http404()
+        return room
+
+
+class RoomPhotosView(user_mixins.LoggedInOnlyView, DetailView):
+
+    model = models.Room
+    template_name = "room_photos.html"
+
+    def get_object(
+        self, queryset=None
+    ):  # url에서 pk를 받는 UpdateView이 소유권 확인이 힘들하는 점을 보완하기 위해
+        room = super().get_object(queryset=queryset)
+
+        if room.host.pk != self.request.user.pk:
+            raise Http404()
+        return room
